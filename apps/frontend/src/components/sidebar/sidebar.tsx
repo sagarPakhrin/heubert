aad3aeffe0581ac06d12c808e3a6e111f35c979e
sagarPakhrin <sagarlama826@gmail.com>
2022-05-04 01:07:23 +0545

    chore: Add sidebar

---
+++ apps/frontend/src/components/sidebar/sidebar.tsx
@@ -0,0 +1,53 @@
+import { FilterIcon, PresentationChartBarIcon } from '@heroicons/react/solid';
+import { NavLink } from 'react-router-dom';
+import { classNames } from '../../utils/class-names';
+
+const Sidebar = () => {
+  const links = [
+    {
+      to: '/leads',
+      label: 'Leads',
+      icon: <FilterIcon className="w-6 h-6" />,
+    },
+    {
+      to: '/reports',
+      label: 'Reports',
+      icon: <PresentationChartBarIcon className="w-6 h-6" />,
+    },
+  ];
+  return (
+    <div
+      className={`pb-8 h-full w-80 bg-gray-900 hidden lg:flex flex-shrink-0 flex-col text-gray-300 overflow-x-hidden overflow-y-auto transition-all delay-75 sidebar
+      'px-7 active'
+    `}
+    >
+      <div
+        className={classNames(
+          'px-6 pt-8 pb-6 flex items-center sticky top-0 z-10 ',
+          'bg-gray-900 justify-start border-b border-gray-800'
+        )}
+      >
+        <h3 className="text-gray-300">Heubert</h3>
+      </div>
+      <div className="mt-8 px-4">
+        {links.map((link, idx) => (
+          <NavLink
+            to={link.to}
+            className={({ isActive }) =>
+              classNames(
+                'my-1 py-3 px-4 rounded flex items-center gap-3',
+                isActive ? 'bg-gray-700' : ''
+              )
+            }
+            key={idx}
+          >
+            {link.icon}
+            <span className="text-sm link-title truncate">{link.label}</span>
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- apps/frontend/src/layouts/default-layout.tsx
+++ apps/frontend/src/layouts/default-layout.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Sidebar from '../components/sidebar';
 
 export const DefaultLayout = () => {
   return (
     <div className="flex bg-gray-50 w-full h-screen">
-      <aside>Sidebar</aside>
+      <Sidebar />
       <div className="overflow-y-auto flex-1 flex flex-col">
         <div className="flex-1 py-12 px-8 2xl:px-32">
           <Outlet />
